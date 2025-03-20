358e59609dee3e41006c10625aeab2103a270cca
SHINDA3iv <102847745+SHINDA3iv@users.noreply.github.com>
2025-03-20 21:57:35 +0700

    Update permissions.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,14 +1,17 @@
 from rest_framework import permissions
 
 
+# Проверка прав доступа
 class OwnershipPermission(permissions.BasePermission):
 
+    # Разрешение на уровне запроса
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated
         )
 
+    # Разрешение на уровне объекта
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
