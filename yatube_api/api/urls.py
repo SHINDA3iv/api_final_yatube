ff1f0866a9978eadc4d5cca7c0dd6281ce0398e9
SHINDA3iv <102847745+SHINDA3iv@users.noreply.github.com>
2025-03-20 21:54:48 +0700

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
 
-
+# Роутеры для автоматической маршрутизации API
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
 router.register(r'groups', GroupViewSet, basename='groups')
@@ -11,6 +11,7 @@ router.register(r'^posts/(?P<post_pk>\d+)/comments', CommentViewSet,
                 basename='comments'
                 )
 
+# Паттерны URL для API и аутентификации
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/', include('djoser.urls')),
