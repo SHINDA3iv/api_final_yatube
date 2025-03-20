8455bc111384be151c5e26f952fda86209e64517
SHINDA3iv <102847745+SHINDA3iv@users.noreply.github.com>
2025-03-20 22:46:29 +0700

    Update urls.py

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+# Основные маршруты проекта
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('admin/', admin.site.urls),  # Админка Django
+    path('api/', include('api.urls')),  # API endpoints
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
+        name='redoc'  # Документация Redoc
     ),
 ]
